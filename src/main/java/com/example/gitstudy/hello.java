0c283300f08712170d7c685d8b8d191d6c41d5c6
chestnut <904777489@qq.com>
2020-12-13 16:49:38 +0800

    second file up

---
+++ src/main/java/com/example/gitstudy/hello.java
@@ -0,0 +1,11 @@
+package com.example.gitstudy;
+
+/**
+ * @author lili
+ * @create 2020-12-13 下午 4:46
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
